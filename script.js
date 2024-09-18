cc97803193f2bab31af2adeded2f839366835bc3
Nitishprasadverma <nitishverma773964@gmail.com>
2024-09-18 18:47:39 +0530

    toggle_theme_done, messages reponsend, copy_clipboard, and many_more

--- script.js
+++ script.js
@@ -1,14 +1,67 @@
 const typingForm = document.querySelector(".typing-form");
 const chatlist = document.querySelector(".chat-list");
+const toggleThemeButton = document.querySelector("#themeButton");
 let userMessage = null;
+const API_KEY = "AIzaSyCL_mOpcCkGar9s4QBgFi2dF3Rms9LA5vg";
+const API_URL = ` https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`
+
 //create a new message element and return it
-const createMessageElement = (content, ...classes) =>{
+const createMessageElement = (content, ...classes) => {
     const div = document.createElement("div");
     div.classList.add("message", ...classes);
     div.innerHTML = content;
     return div;
 }
 
+// show typing effect by displaying words one by one
+const showTypingEffect = (text, textElment) => {
+    const words = text.split(' ');
+    let currentWordIndex = 0;
+    //append each word to the text element with a space;]
+    const typingInterval = setInterval(() => {
+        textElment.innerText += (currentWordIndex === 0 ? '' : ' ') + words[currentWordIndex++];
+
+        //if all words are displayed
+        if (currentWordIndex === words.length) {
+            clearInterval(typingInterval);
+        }
+    }, 80)
+}
+
+//fetch response from api based on user message;
+const generateAPIResponse = async (incomingMessageDiv) => {
+    const textElment = incomingMessageDiv.querySelector(".text");
+
+
+    try {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                contents: [{
+                    role: "user",
+                    parts: [{ text: userMessage }]
+                }]
+            })
+        });
+        const data = await response.json();
+        console.log(data);
+
+        //get the api response text
+        const apiReponse = data?.candidates[0].content.parts[0].text;
+        showTypingEffect(apiReponse, textElment)
+        console.log(apiReponse);
+        // textElment.innerText = apiReponse; 
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+    finally {
+        incomingMessageDiv.classList.remove("loading")
+    }
+}
 const showLoadingAnimation = () => {
     const html = `<div class="message-content">
 
@@ -22,18 +75,29 @@ const showLoadingAnimation = () => {
                     <div class="loading-bar"></div>
                 </div>
             </div>
-            <span class="icon material-symbols-rounded">content_copy</span>`
+            <span onclick = "copyMessage(this)" class="icon material-symbols-rounded">content_copy</span>`
 
-const incomingMessageDiv = createMessageElement(html, "incoming", "loading");
- 
-chatlist.appendChild(incomingMessageDiv);
+    const incomingMessageDiv = createMessageElement(html, "incoming", "loading");
+
+    chatlist.appendChild(incomingMessageDiv);
+    generateAPIResponse(incomingMessageDiv);
+}
+
+// copy message 
+const copyMessage =(copyIcon) =>{
+    const messageText = copyIcon.parentElement.querySelector(".text").innerText;
+
+    navigator.clipboard.writeText(messageText);
+    copyIcon.innerText = "done"; //show tick incon
+
+    setTimeout(() => copyIcon.innerText = "content_copy", 1000)
 }
 
 //handling sending outgoing chat messages
-const handleOutgoingChat = () =>{
+const handleOutgoingChat = () => {
     userMessage = typingForm.querySelector(".typing-input").value.trim()
 
-    if(!userMessage) return
+    if (!userMessage) return
     console.log(userMessage);
     const html = ` <div class="message-content">
 
@@ -43,17 +107,34 @@ const handleOutgoingChat = () =>{
                 
             </div>`
 
-        const outgoingMessageDiv = createMessageElement(html, "outgoing");
+    const outgoingMessageDiv = createMessageElement(html, "outgoing");
 
-        outgoingMessageDiv.querySelector(".text").innerText = userMessage;
-        chatlist.appendChild(outgoingMessageDiv);
+    outgoingMessageDiv.querySelector(".text").innerText = userMessage;
+    chatlist.appendChild(outgoingMessageDiv);
 
-        typingForm.reset() //clear input field
+    typingForm.reset() //clear input field
 }
 
-typingForm .addEventListener("submit", (e) =>{
-e.preventDefault();
+//light mode toggle
 
-handleOutgoingChat();
-showLoadingAnimation();
+toggleThemeButton.addEventListener("click" ,() =>{
+  const isLightMode =  document.body.classList.toggle("light_mode")
+  localStorage.setItem("themecolor", isLightMode ? "light_mode" : "dark_mode")
+    toggleThemeButton.innerText = isLightMode ? "dark_mode" : "light_mode"
+})
+
+const loadLocalStorageData = () =>{
+    const isLightMode = (localStorage.getItem("themecolor") === "light_mode");
+
+    //applying the stored theme color
+    document.body.classList.toggle("light_mode" , isLightMode)
+
+    toggleThemeButton.innerText = isLightMode ? "dark_mode" : "light_mode"
+}
+loadLocalStorageData();
+typingForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    handleOutgoingChat();
+    showLoadingAnimation();
 })
